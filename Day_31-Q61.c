ae8b97c23b09e59f5421434d13a54ba0173d505d
adityaadikumar05-sys <adityaadikumar05@gmail.com>
2025-12-06 12:17:37 +0530

    Update Day_31-Q61.c

--- Day_31-Q61.c
+++ Day_31-Q61.c
@@ -1,35 +1,40 @@
-//Q61: Reverse an array without taking extra space.
+//Q61: Search for an element in an array using linear search.
 
 /*
 Sample Test Cases:
 Input 1:
-4
-1 2 3 4
+5
+1 2 3 4 5
+3
 Output 1:
-4 3 2 1
+Found at index 2
+
+Input 2:
+4
+10 20 30 40
+25
+Output 2:
+-1
 
 */
 
 #include <stdio.h>
 
 int main() {
-    int n = 0;
-    scanf("%d", &n);
+    int n;
+    scanf("%d",&n);
     int arr[n];
     for (int i = 0; i < n; i++) {
         scanf("%d", &arr[i]);
     }
-    int left = 0, right = n-1;
-    while (left <= right) {
-        int temp = arr[left];
-        arr[left] = arr[right];
-        arr[right] = temp;
-        left++;
-        right--;
-    }
+    int input=0;
+    int output = -1;
+    scanf("%d", &input);
     for (int i = 0; i < n; i++) {
-        printf("%d ", arr[i]);
+        if (arr[i] == input) {
+            output = i;
+        }
     }
+    printf("Found at index %d\n", output);
     return 0;
-
 }
